4349f1484497705dfbf4a6dbefec9d8dcc5e47a5
andrewkizito <andrewkizito54@gmail.com>
2024-06-22 09:47:32 +0300

    feat: cleanup

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,25 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={20} name="home-outline" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={20} name="settings-outline" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Hellow Bozo</Text>
     </View>
   );
 }
